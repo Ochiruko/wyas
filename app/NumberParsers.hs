85fb39431e7ab69a4e4b5b69edcec37e407c449d
Dylan Sommer <dylansommer96@gmail.com>
2024-05-21 18:13:59 -0400

    parseComplex complete

--- app/NumberParsers.hs
+++ app/NumberParsers.hs
@@ -23,7 +23,12 @@ parseComplex r =
      do rcmp <- parseReal <|> parseRational <|> parseInteger
         sgn  <- oneOf "+-"
         imcp <- parseUReal <|> parseURational <|> parseUInteger
+        char 'i'
         return . Complex $ case sgn of
           '+' -> toDouble rcmp :+ toDouble imcp
           '-' -> toDouble rcmp :+ (-1 * toDouble rcmp)
- <|> do 
+     -- parse [+|-|empty]bi
+ <|> do icmp <- parseReal <|> parseRational <|> parseInteger
+        char 'i'
+        return . Complex $ icmp
+
