7be50f6ab7cb70e8050dff7c6f2fa40e21620bca
Dylan Sommer <dylansommer96@gmail.com>
2024-05-21 19:49:36 -0400

    wrote parseUReal

--- app/NumberParsers.hs
+++ app/NumberParsers.hs
@@ -9,29 +9,29 @@ data LispNum = Complex (Complex Double)
              | Real Double
              | Rational Integer
              | Integer Integer
-             deriving (Eq, Show, Functor)
+             deriving (Eq, Show)
 
 parseNum :: Parser LispNum
 parseNum = 
   do radix <- parseRadix
-     let ops = [ parseComplex
-               , parseReal
-               , parseRational
-               , parseInteger ]
+     let ops = fmap try [ parseComplex
+                        , parseReal
+                        , parseRational
+                        , parseInteger ]
      choice (ops <*> [radix])
 
 parseComplex :: Integer -> Parser LispNum
 parseComplex r =
      -- parse a+bi
-     do rcmp <- parseReal r <|> parseRational r <|> parseInteger r
+     do rcmp <- choice . fmap try $ [parseReal, parseRational, parseInteger] <*> [r]
         sgn  <- oneOf "+-"
-        imcp <- parseUReal r <|> parseURational r <|> parseUInteger r
+        icmp <- choice . fmap try $ [parseUReal, parseURational, parseUInteger] <*> [r]
         char 'i'
         return . Complex $ case sgn of
-          '+' -> (toDouble <$> rcmp) :+ (toDouble <$> imcp)
-          '-' -> (toDouble <$> rcmp) :+ (negate <$> (toDouble <$> rcmp))
+          '+' -> toDouble rcmp :+ toDouble imcp
+          '-' -> toDouble rcmp :+ negate (toDouble rcmp)
      -- parse [+|-|empty]bi
- <|> do icmp <- parseReal r <|> parseRational r <|> parseInteger r
+ <|> do icmp <- choice . fmap try $ [parseReal, parseRational, parseInteger] <*> [r]
         char 'i'
         return . Complex $ icmp
 
@@ -45,12 +45,20 @@ toDouble x = case x of
 parseReal :: Integer -> Parser LispNum
 parseReal r = 
   do sgn <- oneOf "+-" <|> return '+'
-     dec <- parseUReal 10
+     unum <- parseUReal 10
+     let (Real num) = unum
      return . Real $ case sgn of
-       '+' -> dec
-       '-' -> negate <$> dec
+       '+' -> num
+       '-' -> negate num
        _ -> fail "parsing error: sign not parsed properly"
 
 parseUReal :: Integer -> Parser LispNum
 parseUReal r = 
-     do 
+     do whl <- many digit
+        char '.'
+        dec <- many digit
+        let num = read $ whl ++ "." ++ dec
+        return . Real $ case (length num) of
+          1 -> fail "'.' is an invalid UReal"
+          _ -> num
+
