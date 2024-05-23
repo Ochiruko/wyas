4d19b11d5fccbaed007c7698eda476b1780eb506
Dylan Sommer <dylansommer96@gmail.com>
2024-05-23 16:53:47 -0400

    formatting

--- app/Tests.hs
+++ app/Tests.hs
@@ -14,14 +14,14 @@ runTests :: IO ()
 runTests = hspec $ do
   describe "parseNum" $ do
     it "parses all valid scheme numbers" $ do
-      parse parseNum "lisp" "#x029f3" `shouldBe` (Right $ Integer (3+16*15+9*16^2+2*16^3))
-      parse parseNum "lisp" "23425." `shouldBe` (Right $ Real 23425.0)
-      parse parseNum "lisp" "23425" `shouldBe` (Right $ Integer 23425)
-      parse parseNum "lisp" "234/25" `shouldBe` (Right $ Rational (234 % 25))
+      parse parseNum "lisp" "#x029f3" `shouldBe` Right (Integer (3+16*15+9*16^2+2*16^3))
+      parse parseNum "lisp" "23425." `shouldBe` Right (Real 23425.0)
+      parse parseNum "lisp" "23425" `shouldBe` Right (Integer 23425)
+      parse parseNum "lisp" "234/25" `shouldBe` Right (Rational (234 % 25))
   describe "parseExpr" $ do
     it "parses all valid scheme expressions" $ do
       parse parseExpr "lisp" "(a test)" `shouldBe` 
-        (Right $ List [Atom "a", Atom "test"])
+        Right ( List [Atom "a", Atom "test"])
 --    parse parseExpr "lisp" "(a (nested) test)" `shouldBe`
 --      (Right $ List [Atom "a", List [Atom "nested"], Atom "test"])
 --    parse parseExpr "lisp" "(a '(quoted (dotted . list)) test)" `shouldBe` 
