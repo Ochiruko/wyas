8ce8ee7ef0e23cb725a926a405aa334f83cabc2d
Dylan Sommer <dylansommer96@gmail.com>
2024-05-26 13:58:34 -0400

    Updated Main.hs to fit hindent style

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,16 @@ import Tests
 main :: IO ()
 main = runTests
   
-evalNums = 
-  do putStrLn "testing parseNum."
-     mainLoop
-     where mainLoop = 
-             do putStrLn "Enter a Scheme number: "
-                input <- getLine
-                let output = case parse parseNum "lisp" input of
-                               Left err -> "there was an error: " ++ show err
-                               Right succ -> "success: " ++ show succ
-                putStrLn output
-                mainLoop
+evalNums = do 
+  putStrLn "testing parseNum."
+  mainLoop
+  where 
+    mainLoop = do 
+      putStrLn "Enter a Scheme number: "
+      input <- getLine
+      let output = 
+            case parse parseNum "lisp" input of
+              Left err -> "there was an error: " ++ show err
+              Right succ -> "success: " ++ show succ
+      putStrLn output
+      mainLoop
